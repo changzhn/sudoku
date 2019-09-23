cf7591aafaca332ac4263990d910c23787521077
changzhn <377757708@qq.com>
2019-09-23 11:39:33 +0800

    feat: fill by step

--- src/utils/test.js
+++ src/utils/test.js
@@ -19,16 +19,20 @@ class Utils {
 class Palace {
   constructor(palaceNum) {
     this.palaceNum = palaceNum;
-    this.palaceAvailableNums = [...baseNums];
     this.rowIdxStart = (Math.ceil(palaceNum / 3) - 1) * 3;
     this.rowIdxEnd = this.rowIdxStart + 3;
-    this.colIdxStart = (Math.ceil(palaceNum / 3) - 1) * 3;
+
+    let colRest = palaceNum % 3;
+    if (colRest === 0) {
+      colRest = 3;
+    }
+    this.colIdxStart = (colRest - 1) * 3;
     this.colIdxEnd = this.colIdxStart + 3;
   }
 
-  reduceNum(num) {
-    this.palaceAvailableNums = this.palaceAvailableNums.filter(base => base !== num);
-  }
+  // reduceNum(num) {
+  //   this.palaceAvailableNums = this.palaceAvailableNums.filter(base => base !== num);
+  // }
 
   static calcBelongTo(rowIdx, colIdx) {
     const palaceNum = Math.floor(rowIdx / 3) * 3 + Math.floor(colIdx / 3) + 1;
@@ -49,8 +53,12 @@ class Board {
   constructor() {
     this.palaces = this.generatePalace();
     this.grids = this.generateGrids();
-    this.fillPalace(1, 5, 9);
-    console.log(this.toArray());
+    this.fillPalaceRandom(1, 5, 9);
+    this.fillPalaceStep(2, 3, 4, 6, 7, 8);
+
+    // FIXME:
+    let array = this.toArray();
+    return array;
   }
 
   generatePalace() {
@@ -69,7 +77,7 @@ class Board {
     return grids;
   }
 
-  fillPalace(...palaceNums) {
+  fillPalaceRandom(...palaceNums) {
     palaceNums.forEach(palaceNum => {
       const palace = this.palaces[palaceNum - 1];
       const { rowIdxStart, rowIdxEnd, colIdxStart, colIdxEnd } = palace;
@@ -82,11 +90,53 @@ class Board {
     });
   }
 
+  fillPalaceStep(...palaceNums) {
+    palaceNums.forEach(palaceNum => {
+      const palace = this.palaces[palaceNum - 1];
+      const { rowIdxStart, rowIdxEnd, colIdxStart, colIdxEnd } = palace;
+      for(let i = rowIdxStart; i < rowIdxEnd; i++) {
+        for(let j = colIdxStart; j < colIdxEnd; j++) {
+          const grid = this.grids[i][j];
+          const availabelNums = this.getAvailableNums(grid);
+          if (availabelNums.length) {
+            grid.num = availabelNums[0];
+          }
+        }
+      }
+    });
+  }
+
+  getAvailableNums({ rowIdx: targetRowIdx, colIdx: targetColIdx, belongToPalace }) {
+    const { palaceNum: targetPalaceNum } = belongToPalace;
+    const occupiedRowNums = [];
+    const occupiedColNums = [];
+    const occupiedPalaceNums = [];
+    this.grids.forEach(row => {
+      row.forEach(grid => {
+        if (grid.num) {
+          const { rowIdx, colIdx, palaceNum, num } = grid;
+          if (rowIdx === targetRowIdx) {
+            occupiedRowNums.push(num);
+          }
+          if (colIdx === targetColIdx) {
+            occupiedColNums.push(num);
+          }
+          if (palaceNum === targetPalaceNum) {
+            occupiedPalaceNums.push(num);
+          }
+        }
+      })
+    });
+
+    const occupiedNums = new Set([...occupiedRowNums, ...occupiedColNums, ...occupiedPalaceNums]);
+    const allNums = new Set(baseNums);
+    const availabelNums = [...new Set([...allNums].filter(x => !occupiedNums.has(x)))];
+    return availabelNums;
+  }
+
   toArray() {
-    return this.grids.map(row => {
-      row = row.map(grid => grid.num);
-      return row;
-    })
+    return this.grids.map(row => row.map(grid => grid.num))
   }
 }
-new Board();
+
+console.log(new Board())
